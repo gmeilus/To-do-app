526db62998de80eeeff72be9569c0404a9b5bd96
Gediminas Meilus <gediminasmeilus@Mac.broadband>
2025-12-12 16:16:52 +0000

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,93 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
+import { text } from "stream/consumers";
 
 export default function Home() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const removeTodo = (index) => {setTodos(todos.filter((_,i) => i !==index))};
+
+  const addTodo = (e) => {
+    e.preventDefault(); // prevent page reload
+    if (!input) return;              // skip empty input
+    setTodos([...todos, {text: input, completed: false}]);     // add to list
+    setInput("");                    // clear input
+  };
+
+  const toggleTodo = (index) => {
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } :todo
+      )
+    );
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex  w-full max-w-sm flex-col  py-32  bg-white dark:bg-gray rounded-lg shadow-blue-300 shadow-2xl shadow-[0_20px_60px_rgba(59,130,246,0.5) border-2 border-blue-300">
+        
+          
+        <div className="flex flex-col  gap-6 text-center  ">
+          <h1 className=" text-3xl font-semibold text-center mb-4">
+           To Do app
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+         <div>
+           <form onSubmit={addTodo}>
+          <input 
+          type="text"
+          className="input-field border-2 rounded"
+          id="todoInput"
+          placeholder="Add new to do "
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+           
+          />
+       
+       <button
+        type="submit"
+        className="btn bg-blue-500 text-white px-4 py-1.5 rounded-md shadow-md shadow-blue-300 hover:bg-blue-300 transition"
+        >
+        ADD
+        </button>
+        </form>
+
+
+        <p className="text-center mt-2"><span>{todos.length}</span>{todos.length=== 1  ? "   item" : "   items"}</p>
+
+       
+       
+        <div className=" "></div>
+
+           <ul className="w-full mt-2">
+            {todos.map((todo, index) => (
+              <li
+                key={index}
+                className=" flex justify-between items-center border-b border-gray-200 py-1 px-2order-b border-gray-200 py-1 text-center"
+              >
+                <span
+                onClick={() => toggleTodo(index)}
+                className={`flex-1 cursor-pointer ${todo.completed ? "line-through text-gray-400" : ""}`}
+                >{todo.text}
+                 </span>
+                <button
+                onClick={() => removeTodo(index)}
+                className="ml-2 text-red-500 hover:text-red-700">
+
+                  ❌
+                </button>
+
+              </li>
+            ))}
+          </ul>
+          <button
+          onClick={()=> setTodos([])}
+          className="btn bg-blue-500 text-white px-4 py-1.5 rounded-md shadow-md shadow-blue-300 hover:bg-blue-300 transition">
+            Clear List
+          </button>
+
+
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
         </div>
       </main>
     </div>
